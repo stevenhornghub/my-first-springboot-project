7170e512429d1203a7057a0247ade739bb783df8
Maxi <efmaxi@outlook.com>
2022-07-31 16:32:20 +0800

    1- name column became unique 2- corresponding exception handler for the name column developed

--- src/main/java/com/bib/basiclibraryspring/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/bib/basiclibraryspring/exceptions/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.bib.basiclibraryspring.exceptions;
 
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.Date;
 
 @ControllerAdvice
@@ -27,4 +29,13 @@ public class GlobalExceptionHandler {
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.OK);
     }
+
+    @ExceptionHandler({SQLIntegrityConstraintViolationException.class, DuplicateKeyException.class})
+    public ResponseEntity<?> duplicateKeyExceptionHandler(Exception ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.OK);
+    }
+
+
+
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,7 +4,7 @@ create table books
 (
     id  int auto_increment not null
         primary key,
-    name           varchar(255) not null,
+    name           varchar(255) not null UNIQUE,
     description    varchar(255) not null,
     genre          varchar(255) not null,
     author         varchar(255)  not null
